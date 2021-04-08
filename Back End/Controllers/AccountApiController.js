4c37a77af3d31b5ca9faab3c7164b849b49cc8a0
linhleba <linhleba.it@gmail.com>
2021-04-08 16:01:18 +0700

    update controllers mvc

--- Back End/Controllers/AccountApiController.js
+++ Back End/Controllers/AccountApiController.js
@@ -1,1 +1,27 @@
-//test git
+var account = require("../Models/Account")
+module.exports = {
+
+  login: function(req,res)
+  {
+    console.log(req.query.username)
+    console.log(req.query.password)
+    var username = req.query.username;
+    var password = req.query.password;
+
+
+    account.Login
+    (
+        username,
+        password,
+        function(rs)
+        {
+            res.send(JSON.stringify(rs))
+        }
+    )
+  },
+
+  update: function(req,res)
+  {
+    console.log("this is an area for update account service")
+  }
+};

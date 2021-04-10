3593bd37d094f8cabfb5a8a345a65f2fda421c49
linhleba <linhleba.it@gmail.com>
2021-04-10 11:38:59 +0700

    update code in model

--- Back End/Models/Account.js
+++ Back End/Models/Account.js
@@ -5,30 +5,17 @@ exports.Login = function(username, password, callbackQuerry){
     db.connect();
     var result = false;
 
-    //var sqlString = "Call USP_Login('"+ username + "','"+password+"')";
     var sqlString="Select * from account where username ='"+username+"'";
 
     var conn = db.getConnection();
     conn.query(sqlString,
     function(err, rs, fields)
     {
-        if(!err){
-            //console.log(rs[0]);
-            if(rs.length>0)
-            {
-                if(rs[0].password == password){
-                    result = true;
-                }
-                else{
-                    result=false;
-                }
-            }
-            else {
-                rs=false;
-            }
-            callbackQuerry(result);
+        if (err) {
+          console.log("Nhay vao day");
+          return;
         }
-        else
-            console.log("Nhay vao day");
+        result = (rs, password) => rs.length > 0  && rs[0].password == password;
+        callbackQuerry(result);
     })
 }
--- Back End/main.js
+++ Back End/main.js
@@ -1,14 +1,10 @@
 var express = require("express")
 var AccountApi = require("./Routes/AccountApi")
-
 //init server by express
 var app = express();
-
 //config port
 var port = 1234;
-
 app.get("/", (req, res) => res.send("Hello Express"))
-
 //handle all api here
 AccountApi(app);
 app.listen(port);
